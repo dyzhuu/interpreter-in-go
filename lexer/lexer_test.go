eb124c43931f71ced281b85923b367f77a0d678c
dyzhuu <119282717+dyzhuu@users.noreply.github.com>
2024-10-25 19:05:55 +1300

    feat: 1.3 * added initial token  + lexer with basic tests

---
+++ lexer/lexer_test.go
@@ -0,0 +1,104 @@
+package lexer
+
+import (
+	"main/token"
+	"testing"
+)
+
+type TestInput = struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []TestInput{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		testToken := l.NextToken()
+
+		if testToken.Type != tt.expectedType {
+			t.Fatalf("test[%d] failed - TokenType wrong. Expected=%q, got=%q", i, tt.expectedType, testToken.Type)
+		}
+
+		if testToken.Literal != tt.expectedLiteral {
+			t.Fatalf("test[%d] failed - TokenLiteral wrong. Expected=%q, got=%q", i, tt.expectedLiteral, testToken.Literal)
+		}
+	}
+}
+
+func TestNextToken2(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+let add = fn(x, y) {
+x + y;
+};
+let result = add(five, ten);
+`
+	tests := []TestInput{
+		{token.LET, "let"},
+		{token.IDENTIFIER, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENTIFIER, "x"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENTIFIER, "x"},
+		{token.PLUS, "+"},
+		{token.IDENTIFIER, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENTIFIER, "add"},
+		{token.LPAREN, "("},
+		{token.IDENTIFIER, "five"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		testToken := l.NextToken()
+
+		if testToken.Type != tt.expectedType {
+			t.Fatalf("test[%d] failed - TokenType wrong. Expected=%q, got=%q", i, tt.expectedType, testToken.Type)
+		}
+
+		if testToken.Literal != tt.expectedLiteral {
+			t.Fatalf("test[%d] failed - TokenLiteral wrong. Expected=%q, got=%q", i, tt.expectedLiteral, testToken.Literal)
+		}
+	}
+}
---
+++ token/token.go
@@ -0,0 +1,46 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers & literals
+	IDENTIFIER = "IDENTIFIER"
+	INT        = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if keywordToken, ok := keywords[ident]; ok {
+		return keywordToken
+	}
+	return IDENTIFIER
+}
